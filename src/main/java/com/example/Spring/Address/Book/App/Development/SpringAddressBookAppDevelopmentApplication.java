22ff464a346e712bb5346c7f84ac86c6d10641c2
Samiksha <maahit925@gmail.com>
2025-03-06 20:31:19 +0530

    main

---
+++ src/main/java/com/example/Spring/Address/Book/App/Development/SpringAddressBookAppDevelopmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Address.Book.App.Development;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAddressBookAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAddressBookAppDevelopmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring/Address/Book/App/Development/SpringAddressBookAppDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Address.Book.App.Development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAddressBookAppDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
